d685129f1ff626280cc1c77fab2f60f625520fef
Matvey Kuzmin <qjlf@inbox.ru>
2023-07-08 23:05:02 +0300

    init

---
+++ src/main/java/com/example/cursework2/Cursework2Application.java
@@ -0,0 +1,13 @@
+package com.example.cursework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursework2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cursework2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cursework2/Cursework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursework2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
